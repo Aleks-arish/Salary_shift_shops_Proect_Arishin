66a63c33770605c5ca1b279426a014f800f8c7c9
Aleks-arish <113817938+Aleks-arish@users.noreply.github.com>
2023-02-20 22:55:06 +0300

    Скрипт схем БД

---
+++ Создание схем БД 01.sql
@@ -0,0 +1,10 @@
+
+CREATE SCHEMA [HR];--кадровые данные
+GO
+CREATE SCHEMA [RS];--данные систем вознаграждений
+GO
+CREATE SCHEMA [DS];--данные продаж
+GO
+CREATE SCHEMA [AWH];--Данные по учёту рабочего времени
+GO
+CREATE SCHEMA [SWS];--Зарплата по рабочим сменам
